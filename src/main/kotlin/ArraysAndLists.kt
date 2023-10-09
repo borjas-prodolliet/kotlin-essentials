b7fcbfe7ab08ba23bc1fc1b991c7f57d9df600bc
Borjas Prodolliet <borjas.prodolliet@gmail.com>
2023-10-09 10:54:48 -0300

    arrays and lists

--- src/main/kotlin/ArraysAndLists.kt
+++ src/main/kotlin/ArraysAndLists.kt
@@ -16,5 +16,51 @@ fun main() {
 
     println(names.contentToString())
 
+    val arrayOfNulls = arrayOfNulls<String>(5)
 
+    arrayOfNulls.fill("*")
+
+    arrayOfNulls[3] = "Hello"
+
+    println(arrayOfNulls.contentToString())
+
+
+    // lists
+    val namesList: List<String> = listOf(
+        "Borjas",
+        "Adri",
+        "Peter",
+        "Alfonso"
+    )
+
+    println(namesList[2])
+    println(namesList.size)
+    println(namesList.contains("Borjas"))
+    println(namesList.contains("Foo"))
+    println(namesList.first())
+    println(namesList.last())
+    println(namesList.indexOf("Borjas"))
+    println(namesList)
+
+    // mutable lists
+    val namesListMutable = mutableListOf(
+        "Borjas",
+        "Adri",
+        "Peter",
+        "Alfonso"
+    )
+
+
+    println(namesListMutable)
+    namesListMutable.remove("Borjas")
+    println(namesListMutable)
+    namesListMutable.add("Alex")
+    println(namesListMutable)
+    val emptyList = mutableListOf<Any>()
+    println(emptyList.isEmpty())
+
+
+    // destructuring
+    val (one, two, _, four) = namesList
+    println("$one $two $four")
 }
